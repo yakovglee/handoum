82ffe1c0ca83a1baa160e78fc9498b2b5b48686e
yakovglee <yakovglee@gmail.com>
2025-10-08 19:04:53 +0900

    refactor

--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -1,25 +1,5 @@
-import { Badge } from "@/components/ui/badge";
-import {
-  Card,
-  CardAction,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
-import {
-  Item,
-  ItemActions,
-  ItemContent,
-  ItemDescription,
-  ItemGroup,
-  ItemHeader,
-  ItemMedia,
-  ItemSeparator,
-  ItemTitle,
-} from "@/components/ui/item";
-
 import type { Data } from "@/utils/types";
+import WordCard from "./word-card";
 
 interface SectionCardsProps {
   data: Data[];
@@ -29,31 +9,7 @@ export function SectionCards({ data }: SectionCardsProps) {
   return (
     <div className="*:data-[slot=card]:from-primary/70 *:data-[slot=card]:to-secondary/30 grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       {data.map((item, index) => (
-        <Card key={index} className="@container/card">
-          <CardHeader>
-            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-              {item.word.surface}
-            </CardTitle>
-            <CardAction>
-              <Badge variant="outline">{item.word.pos}</Badge>
-            </CardAction>
-          </CardHeader>
-
-          {item.parsed_expression &&
-            item.parsed_expression?.map((item, index) => (
-              <>
-                <Item key={index} className="px-6">
-                  <ItemHeader>
-                    <ItemTitle className="text-md tabular-nums @[250px]/card:text-3xl">{item.lemma}</ItemTitle>
-                    <ItemActions>
-                      <Badge variant="outline">{item.pos}</Badge>
-                    </ItemActions>
-                  </ItemHeader>
-                  <ItemSeparator />
-                </Item>
-              </>
-            ))}
-        </Card>
+        <WordCard key={index} item={item} />
       ))}
     </div>
   );
---
+++ src/components/word-card.tsx
@@ -0,0 +1,46 @@
+import { Badge } from "@/components/ui/badge";
+import { Card, CardAction, CardHeader, CardTitle } from "@/components/ui/card";
+
+import {
+  Item,
+  ItemActions,
+  ItemHeader,
+  ItemSeparator,
+  ItemTitle,
+} from "@/components/ui/item";
+import type { Data } from "@/utils/types";
+
+interface WordCardProps {
+  item: Data;
+}
+function WordCard({ item }: WordCardProps) {
+  return (
+    <Card className="@container/card">
+      <CardHeader>
+        <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+          {item.word.surface}
+        </CardTitle>
+        <CardAction>
+          <Badge variant="outline">{item.word.pos}</Badge>
+        </CardAction>
+      </CardHeader>
+
+      {item.parsed_expression &&
+        item.parsed_expression?.map((item, index) => (
+          <Item key={index} className="px-6">
+            <ItemHeader>
+              <ItemTitle className="text-md tabular-nums @[250px]/card:text-3xl">
+                {item.lemma}
+              </ItemTitle>
+              <ItemActions>
+                <Badge variant="outline">{item.pos}</Badge>
+              </ItemActions>
+            </ItemHeader>
+            <ItemSeparator />
+          </Item>
+        ))}
+    </Card>
+  );
+}
+
+export default WordCard;
