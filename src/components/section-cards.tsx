3115bbcda1b3db06bde40d3cb504724f5aed2084
yakovglee <yakovglee@gmail.com>
2025-10-08 19:17:47 +0900

    add grammar card

--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -1,16 +1,36 @@
 import type { Data } from "@/utils/types";
 import WordCard from "./word-card";
-
+import { Separator } from "@/components/ui/separator";
 interface SectionCardsProps {
   data: Data[];
 }
 
 export function SectionCards({ data }: SectionCardsProps) {
+  const grammar = [
+    ...new Map(
+      data.filter((d) => /^[JEX]/.test(d.word.pos)).map((d) => [d.word.pos, d])
+    ).values(),
+  ].sort((a, b) => a.word.pos.localeCompare(b.word.pos));
+
+  const complicated = [
+    ...new Map(
+      data
+        .filter((d) => d.word.entry_type != "*")
+        .map((d) => [d.word.entry_type, d])
+    ).values(),
+  ].sort((a, b) => a.word.entry_type.localeCompare(b.word.entry_type));
+
   return (
     <div className="*:data-[slot=card]:from-primary/70 *:data-[slot=card]:to-secondary/30 grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
-      {data.map((item, index) => (
+      <Separator className="my-4 bg-primary/30" />
+      {grammar.map((item, index) => (
         <WordCard key={index} item={item} />
       ))}
+      <Separator className="my-4 bg-primary/30" />
+      {complicated.map((item, index) => (
+        <WordCard key={index} item={item} />
+      ))}
+      <Separator className="my-4 bg-primary/30" />
     </div>
   );
 }
