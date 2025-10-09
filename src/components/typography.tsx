3429633363372fd2cddc50478c3abcdb88fe53f4
yakovglee <yakovglee@gmail.com>
2025-10-09 13:46:56 +0900

    add sticky to text

--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -1,3 +1,11 @@
-export function TypographyLarge({children}) {
-  return <div className="text-lg font-semibold px-4 sticky top-0 bg-card p-2">{children}</div>
+interface TypographyLargeProps {
+  children: React.ReactNode;
+}
+
+export function TypographyLarge({ children }: TypographyLargeProps) {
+  return (
+    <div className="text-lg font-semibold px-4 sticky top-0 bg-card p-2 max-h-[30vh] overflow-y-auto rounded-md shadow-sm">
+      {children}
+    </div>
+  );
 }
