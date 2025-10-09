c0dd8ce313e9a7010923bf8b708d808d0df3d2b0
yakovglee <yakovglee@gmail.com>
2025-10-09 13:39:09 +0900

    add sticky to text

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { data } from "./utils/response";
 
 function App() {
   return (
-    <div className="flex flex-1 flex-col">
+    <div className="flex flex-1 flex-col relative">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
           <TextareaWithButton />
--- src/components/typography.tsx
+++ src/components/typography.tsx
@@ -1,3 +1,3 @@
 export function TypographyLarge({children}) {
-  return <div className="text-lg font-semibold px-4">{children}</div>
+  return <div className="text-lg font-semibold px-4 sticky top-0 bg-card p-2">{children}</div>
 }
